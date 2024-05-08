157a20f3810b3bbea44d1f8c466eec7c530868e0
Gulilil <juan.csantoso@gmail.com>
2024-05-08 16:30:34 +0700

    refactor code

--- app/src/main/java/com/sioptik/main/HasilPemrosesan.kt
+++ app/src/main/java/com/sioptik/main/HasilPemrosesan.kt
@@ -12,6 +12,7 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import com.sioptik.main.box_processor.BoxProcessor
 import com.sioptik.main.image_processing_integration.JsonFileAdapter
 import com.sioptik.main.image_processing_integration.JsonTemplateFactory
 import com.sioptik.main.image_processing_integration.OcrMock
@@ -90,13 +91,14 @@ class HasilPemrosesan : AppCompatActivity() {
             }
 
             try {
+                val boxProcessor = BoxProcessor()
                 val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
 
                 // Process Image
-                val detectedBoxes = detectBoxes(bitmap, boxesData!!)
-                val croppedBoxes = cropBoxes(bitmap, detectedBoxes)
+                val detectedBoxes = boxProcessor.detectBoxes(bitmap, boxesData!!)
+                val croppedBoxes = boxProcessor.cropBoxes(bitmap, detectedBoxes)
 
-                Log.i("TEST NEEDED BOXES", "Needed Boxes: ${boxesData.data.num_of_boxes}")
+                Log.i("TEST NEEDED BOXES", "Needed Boxes: ${boxesData?.data?.num_of_boxes}")
                 Log.i("TEST DETECTED BOXES", "Detected Boxes: ${detectedBoxes.size}")
 
                 val ocrResults = processBoxes(croppedBoxes)
@@ -181,124 +183,15 @@ class HasilPemrosesan : AppCompatActivity() {
             Log.i("Tesseract", "'$lang.traineddata' already exists no need to copy")
         }
     }
-}
 
-private fun processImage(bitmap: Bitmap, boxes: List<Rect>, ocrResults: List<String>): Bitmap {
-    val imgProcessor = ImageProcessor()
-    val originalMat = imgProcessor.convertBitmapToMat(bitmap)
-    val processedMat = imgProcessor.preprocessImage(originalMat)
-    val resultImage = imgProcessor.visualizeContoursAndRectangles(processedMat, boxes, Scalar(255.0, 0.0, 0.0), Scalar(255.0, 255.0, 0.0), ocrResults, 4)
-    return imgProcessor.convertMatToBitmap(resultImage)
-}
-
-private fun cropBoxes(bitmap: Bitmap, boxes: List<Rect>): List<Bitmap> {
-    val imgProcessor = ImageProcessor()
-    val originalMat = imgProcessor.convertBitmapToMat(bitmap)
-    return boxes.map { box ->
-        val newMat = Mat(originalMat, box)
-        imgProcessor.convertMatToBitmap(newMat)
+    private fun processImage(bitmap: Bitmap, boxes: List<Rect>, ocrResults: List<String>): Bitmap {
+        val imgProcessor = ImageProcessor()
+        val originalMat = imgProcessor.convertBitmapToMat(bitmap)
+        val processedMat = imgProcessor.preprocessImage(originalMat)
+        val resultImage = imgProcessor.visualizeContoursAndRectangles(processedMat, boxes, Scalar(255.0, 0.0, 0.0), Scalar(255.0, 255.0, 0.0), ocrResults, 4)
+        return imgProcessor.convertMatToBitmap(resultImage)
     }
 }
 
 
-private fun detectBoxes (bitmap: Bitmap, boxesData: BoxMetadata) : List<Rect> {
-
-    val imgProcessor = ImageProcessor()
-    val originalMat = imgProcessor.convertBitmapToMat(bitmap)
-    val processedMat = imgProcessor.preprocessImage(originalMat)
-
-    // Detect Boxes
-    var boxesContainer = mutableListOf<Rect>()
-    val w = bitmap.width
-    val h = bitmap.height
-    val ref_w = boxesData.w_ref
-    val ref_h = boxesData.h_ref
-    val w_ratio = w.toFloat() / ref_w.toFloat()
-    val h_ratio = h.toFloat() / ref_h.toFloat()
-
-    val boxesRectData = boxesData.data.boxes
-    val searchPadding = 150;
-    boxesRectData.forEach { boxData ->
-        val temp_x = (boxData.x * w_ratio).toInt()
-        val temp_y = (boxData.y * h_ratio).toInt()
-        val temp_w = (boxData.w * w_ratio).toInt()
-        val temp_h = temp_w
-        val testRect = Rect(temp_x, temp_y, temp_w, temp_h) // Only for testing
-
-        val adjusted_x = if (temp_x - searchPadding > 0) (temp_x - searchPadding) else 0
-        val adjusted_y = if (temp_y - searchPadding > 0) (temp_y - searchPadding) else 0
-
-        val end_x = temp_x + temp_w + searchPadding
-        val adjusted_w = if (end_x > w) (w - adjusted_x) else (end_x - adjusted_x)
-        val end_y = temp_y + temp_h + searchPadding
-        val adjusted_h = if (end_y > h) (h - adjusted_y) else (end_y - adjusted_y)
-
-        val searchingRect: Rect = Rect(adjusted_x, adjusted_y, adjusted_w, adjusted_h)
-
-        // Crop the searching area
-        val searchingMat: Mat = Mat(processedMat, searchingRect)
-        val boxes = imgProcessor.detectBoxes(searchingMat)
-
-        // All-in
-        boxes.forEach { rect ->
-            val adjustingRect : Rect = Rect((rect.x + adjusted_x), (rect.y + adjusted_y), rect.width, rect.height)
-            boxesContainer.add(adjustingRect)
-        }
-
-    }
-
-    // Eliminate Redundant Boxes
-    val similarityCleansedBoxesContainer = eliminateRedundantBoxes(boxesContainer)
-    // Eliminate y_level isolated Boxes
-    val isolatedCleansedBoxesContainer = eliminateIsolatedBoxes(similarityCleansedBoxesContainer)
-
-    return isolatedCleansedBoxesContainer
-}
-
-private fun eliminateRedundantBoxes(boxes :List<Rect>) : List<Rect>{
-    val boxesContainer = mutableListOf<Rect>()
-    val differenceThreshold = 20
-    val scaleThreshold = 10
-    boxes.forEach { checkingRect ->
-        var foundSimilar = false
-        boxesContainer.forEachIndexed { index, iteratingRect ->
-            val checkSimilarity = abs(iteratingRect.x - checkingRect.x) < differenceThreshold && abs(iteratingRect.y - checkingRect.y) < differenceThreshold
-            if (checkSimilarity){
-                val newWidth = if (checkingRect.width > iteratingRect.width) checkingRect.width else iteratingRect.width
-                val newHeight = if (checkingRect.height > iteratingRect.height) checkingRect.height else iteratingRect.height
-                val newRect = Rect((iteratingRect.x + checkingRect.x)/2, (iteratingRect.y + checkingRect.y)/2, newWidth, newHeight)
-                boxesContainer[index] = newRect
-                foundSimilar = true
-            }
-        }
-        // If there are no boxes with similarity
-        if (!foundSimilar) {
-            boxesContainer.add(checkingRect)
-        }
-    }
-    return boxesContainer
-}
-
-private fun eliminateIsolatedBoxes(boxes : List<Rect>) : List<Rect> {
-    val boxesContainer = mutableListOf<Rect>()
-    val differenceThreshold = 20
-    val notIsolatedThreshold = 1
-    boxes.forEachIndexed { checkingIdx, checkingRect ->
-        var notIsolatedCount = 0
-        boxes.forEachIndexed{ iteratingIdx, iteratingRect ->
-            // Make sure that those Rects are not the same Rect
-            if (iteratingIdx != checkingIdx){
-                val checkNotIsolated = abs(iteratingRect.y - checkingRect.y) < differenceThreshold
-                if (checkNotIsolated){
-                    notIsolatedCount++
-                }
-            }
-        }
-
-        if (notIsolatedCount >= notIsolatedThreshold){
-            boxesContainer.add(checkingRect)
-        }
-    }
-    return boxesContainer
-}
 
--- app/src/main/java/com/sioptik/main/ValidasiGambar.kt
+++ app/src/main/java/com/sioptik/main/ValidasiGambar.kt
@@ -15,7 +15,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.sioptik.main.apriltag.AprilTagDetection
+import com.sioptik.main.apriltag.AprilTagFunction
 import com.sioptik.main.apriltag.AprilTagNative
+import com.sioptik.main.border_processor.BorderProcessor
 import com.sioptik.main.camera_processor.CameraProcessor
 import com.sioptik.main.image_processor.ImageProcessor
 import com.sioptik.main.processing_result.FullScreenImageActivity
@@ -70,10 +72,12 @@ class ValidasiGambar : AppCompatActivity() {
 
     private fun processImage(imageUri: Uri, imageView: ImageView, progressBar: ProgressBar, loadingOverlayBg: View, apriltagTagView: Button, sendButton: Button) {
         try {
+            var borderProcessor = BorderProcessor()
+            val aprilTagFunction = AprilTagFunction()
             val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
-            val borders = processBorderDetection(bitmap)
-            val processedBitmap = processAndCropImage(bitmap, borders)
-            val apriltag = processAprilTagDetection(processedBitmap)
+            val borders = borderProcessor.processBorderDetection(this, bitmap)
+            val processedBitmap = borderProcessor.processAndCropImage(bitmap, borders)
+            val apriltag = aprilTagFunction.processAprilTagDetection(processedBitmap)
 
             runOnUiThread {
                 if (apriltag != null) {
@@ -135,144 +139,5 @@ class ValidasiGambar : AppCompatActivity() {
         }
     }
 
-
-
-    private fun processBorderDetection(bitmap: Bitmap) : List<Rect> {
-        val imgProcessor = ImageProcessor()
-        val borderImageDrawable = R.drawable.border_smaller
-        val borderImageSmallDrawable = R.drawable.border_smallest
-        val borderImage = imgProcessor.loadDrawableImage(this, borderImageDrawable)
-        val borderImageSmall = imgProcessor.loadDrawableImage(this, borderImageSmallDrawable)
-
-        // Initial Mat
-        val originalMat = imgProcessor.convertBitmapToMat(bitmap)
-
-        // SplittedImages
-        val splittedImagesRects = imgProcessor.splitImageRects(originalMat)
-        val splittedImages = imgProcessor.splitImage(originalMat)
-        val borderContainer = mutableListOf<Rect>()
-
-        splittedImages.forEachIndexed { index, mat ->
-            // Detect Borders
-            var border = imgProcessor.detectBorder(imgProcessor.convertToGray(mat), imgProcessor.convertToGray(borderImage))
-            if (border == null){
-                border = imgProcessor.detectBorder(imgProcessor.convertToGray(mat), imgProcessor.convertToGray(borderImageSmall))
-            }
-
-            if (border != null){
-                // Assume that it will only get 1
-                val currentRect = splittedImagesRects[index]
-                val adjustedBorder = Rect((currentRect.x + border.x), (currentRect.y + border.y), border.width, border.height)
-                borderContainer.add(adjustedBorder)
-            } else {
-                Log.i("TEST BORDER DETECTION", "Border Not Found")
-            }
-        }
-        return borderContainer
-    }
-
-    private fun processAndCropImage(bitmap: Bitmap, borderContainer : List<Rect>): Bitmap {
-        val imgProcessor = ImageProcessor()
-
-        // Initial Mat
-        val originalMat = imgProcessor.convertBitmapToMat(bitmap)
-        val resultImage = imgProcessor.visualizeContoursAndBorders(originalMat, borderContainer, Scalar(0.0, 255.0, 255.0), 6) // Comment out this line to see processed image
-
-        // Crop
-        var croppedResultImage = resultImage
-        if (borderContainer.size == 4){
-            val padding = 30;
-            val w = originalMat.width()
-            val h = originalMat.height()
-            val tl_rect = borderContainer[0]
-            val tr_rect = borderContainer[1]
-            val bl_rect = borderContainer[2]
-            val br_rect = borderContainer[3]
-
-            // Adjustment By Checking Four corners
-            // Four corners might be difference in X and Y
-            var tlx = if (tl_rect.x <= bl_rect.x) tl_rect.x else bl_rect.x
-            var tly = if (tl_rect.y <= tr_rect.y) tl_rect.y else tr_rect.y
-            var brx = if (br_rect.x >= tr_rect.x) br_rect.x else tr_rect.x
-            var bry = if (br_rect.y >= bl_rect.y) br_rect.y else bl_rect.y
-
-            // Adjustment if it is beyond the limit of the image
-            tlx = if (tlx - padding <= 0) 0 else tlx- padding
-            tly = if (tly - padding <= 0) 0 else tly - padding
-            brx = if (brx + (br_rect.width) + padding >= w) w else brx + (br_rect.width) + padding
-            bry = if (bry + (br_rect.height) + padding >= h) h else bry + (br_rect.height) + padding
-//            Log.i("TEST BOUNDARIES", tlx.toString() + "||" + tly.toString() + "||" + brx.toString() + "||" + bry.toString())
-
-            croppedResultImage = Mat(resultImage, Rect(tlx, tly, (brx - tlx), (bry - tly)))
-        }
-        return imgProcessor.convertMatToBitmap(croppedResultImage)
-    }
-
-    private fun processAprilTagDetection (bitmap: Bitmap) : AprilTagDetection? {
-        try {
-            val width = bitmap.width
-            val height = bitmap.height
-            val byteArray = getNV21(width, height/4, bitmap)
-            val detections : ArrayList<AprilTagDetection> = AprilTagNative.apriltag_detect_yuv(byteArray, width, height)
-
-            val apriltag = detections[0]
-            return apriltag
-        } catch (e: Exception){
-            return null
-        }
-    }
-
-
-    fun getNV21(inputWidth: Int, inputHeight: Int, scaled: Bitmap): ByteArray? {
-        var scaled = scaled
-        val argb = IntArray(inputWidth * inputHeight)
-
-        // Ensure that the bitmap is in ARGB_8888 format
-        scaled = scaled.copy(Bitmap.Config.ARGB_8888, true)
-        scaled.getPixels(argb, 0, inputWidth, 3 * (inputWidth / 4) - 1, 0, inputWidth / 4, inputHeight)
-        val yuv = ByteArray(inputWidth * inputHeight * 3 / 2)
-        encodeYUV420SP(yuv, argb, inputWidth, inputHeight)
-        scaled.recycle()
-        return yuv
-    }
-
-    fun encodeYUV420SP(yuv420sp: ByteArray, argb: IntArray, width: Int, height: Int) {
-        try {
-            val frameSize = width * height
-            var yIndex = 0
-            var uvIndex = frameSize
-            var a: Int
-            var R: Int
-            var G: Int
-            var B: Int
-            var Y: Int
-            var U: Int
-            var V: Int
-            var index = 0
-            for (j in 0 until height) {
-                for (i in 0 until width) {
-                    a = argb[index] and -0x1000000 shr 24 // a is not used obviously
-                    R = argb[index] and 0xff0000 shr 16
-                    G = argb[index] and 0xff00 shr 8
-                    B = argb[index] and 0xff shr 0
-
-                    // well known RGB to YUV algorithm
-                    Y = (66 * R + 129 * G + 25 * B + 128 shr 8) + 16
-                    U = (-38 * R - 74 * G + 112 * B + 128 shr 8) + 128
-                    V = (112 * R - 94 * G - 18 * B + 128 shr 8) + 128
-
-                    // NV21 has a plane of Y and interleaved planes of VU each sampled by a factor of 2
-                    //    meaning for every 4 Y pixels there are 1 V and 1 U.  Note the sampling is every other
-                    //    pixel AND every other scanline.
-                    yuv420sp[yIndex++] = (if (Y < 0) 0 else if (Y > 255) 255 else Y).toByte()
-                    if (j % 2 == 0 && index % 2 == 0) {
-                        yuv420sp[uvIndex++] = (if (V < 0) 0 else if (V > 255) 255 else V).toByte()
-                        yuv420sp[uvIndex++] = (if (U < 0) 0 else if (U > 255) 255 else U).toByte()
-                    }
-                    index++
-                }
-            }
-        } catch (e: IndexOutOfBoundsException) { }
-    }
 
 }
